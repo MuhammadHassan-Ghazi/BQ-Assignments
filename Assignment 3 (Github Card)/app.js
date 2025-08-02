29a2998e9100380c22bd8328b8ac1fc946ada6a7
MuhammadHassan-Ghazi <muhammadhassanyousufghazi@gmail.com>
2025-08-02 12:44:37 +0500

    Assignment 3 (Github Card)

---
+++ Assignment 3 (Github Card)/app.js
@@ -0,0 +1,71 @@
+/*function searchUser() {
+    const username = document.getElementById("usernameInput").value.trim();
+    const card = document.getElementById("profileCard");
+  
+    if (!username) {
+      alert("Please enter a GitHub username.");
+      return;
+    }
+  
+    fetch(`https://api.github.com/users/${username}`)
+      .then(response => {
+        if (!response.ok) {
+          throw new Error("User not found");
+        }
+        return response.json();
+      })
+      .then(data => {
+        card.innerHTML = `
+          <div class="profile-info">
+            <h2>${data.name || "No name provided"}</h2>
+            <p><strong>Username:</strong> <br>${data.login}</p>
+            <p><strong>Bio:</strong> <br>${data.bio || "No bio available"}</p>
+            <p><strong>Location:</strong> <br>${data.location || "Not specified"}</p>
+            <p><strong>Public Repos:</strong> <br>${data.public_repos}</p>
+            <p><a href="${data.html_url}" target="_blank">View GitHub Profile</a></p>
+          </div>
+          <img src="${data.avatar_url}" alt="${data.login}'s avatar" />
+        `;
+        card.style.display = "flex";
+        card.style.padding = "20px";
+      })
+      .catch(error => {
+        card.innerHTML = `<p style="color:red;">${error.message}</p>`;
+        card.style.display = "block";
+      });
+  }*/
+
+async function searchUser() {
+    const username = document.getElementById("usernameInput").value.trim();
+    const card = document.getElementById("profileCard");
+  
+    if (!username) {
+      card.innerHTML = `<p style="color:red;">${"Please enter a GitHub username."}</p>`;
+      card.style.display = "block";
+      card.style.padding = "20px";
+      return;
+    }
+
+    const userInfo = await fetch(`https://api.github.com/users/${username}`);
+    if(!userInfo.ok){
+        card.innerHTML = `<p style="color:red;">${"User not found"}</p>`;
+        card.style.display = "block";
+        card.style.padding = "20px";
+        return;
+    }
+        
+    const data = await userInfo.json()
+    card.innerHTML = `
+          <div class="profile-info">
+            <h2>${data.name || "No name provided"}</h2>
+            <p><strong>Username:</strong> <br>${data.login}</p>
+            <p><strong>Bio:</strong> <br>${data.bio || "No bio available"}</p>
+            <p><strong>Location:</strong> <br>${data.location || "Not specified"}</p>
+            <p><strong>Public Repos:</strong> <br>${data.public_repos}</p>
+            <p><a href="${data.html_url}" target="_blank">View GitHub Profile</a></p>
+          </div>
+          <img src="${data.avatar_url}" alt="${data.login}'s avatar" />`;
+
+    card.style.display = "flex";
+    card.style.padding = "20px";
+}

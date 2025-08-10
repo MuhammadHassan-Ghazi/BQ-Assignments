829e4326693b7281bb127ad64844453eb18a2729
MuhammadHassan-Ghazi <muhammadhassanyousufghazi@gmail.com>
2025-08-10 15:06:42 +0500

    modify bulb toogle

--- Assignment 4/React-Toggle-Bulb/src/App.jsx
+++ Assignment 4/React-Toggle-Bulb/src/App.jsx
@@ -14,19 +14,19 @@ export default function App() {
         
           src="https://5.imimg.com/data5/WL/SG/MY-8835990/10w-gls-light-bulb.jpg"
           alt="Bulb On"
-          className= "w-70 h-80 object-contain "
+          className= "w-72 h-80 object-contain "
         />
       ) : (
         <img
           src="https://thumbs.dreamstime.com/b/    d-illustration-empty-light-bulb-isolated-white-background-realistic-d-rendering-electric-lamp-inside-d-103349315.jpg"
           alt="Bulb Off"
-          className= "w-70 h-80 object-contain"
+          className= "w-72 h-80 object-contain"
         />
       )}
 
       <button
         onClick={toggle}
-        className="px-6 py-3 rounded-lg bg-yellow-400 hover:bg-yellow-500 text-white font-semibold"
+         className="px-6 py-3 rounded-lg bg-stone-800 hover:bg-yellow-500 text-white font-bold"
       >
         {onBulb ? "Turn Off" : "Turn On"}
       </button>
